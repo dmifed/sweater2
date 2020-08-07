872f87a9da30f0527af1d05b3a79d71402824548
dmifed <dmitriy_fedorov@list.ru>
2020-08-07 20:10:17 +0400

    Initial commit

---
+++ src/main/java/com/dmifed/sweater/Application.java
@@ -0,0 +1,13 @@
+package com.dmifed.sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by DIMA, on 07.08.2020
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/dmifed/sweater/GreetingController.java
@@ -0,0 +1,31 @@
+package com.dmifed.sweater;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+/**
+ * Created by DIMA, on 07.08.2020
+ */
+// Identify class as Controller
+@Controller
+public class GreetingController {
+    // Controller is a program module which listening request at specific path ("/greeting") and response data.
+    // Now it should be response a template file
+    @GetMapping("/greeting") //This annotation ensures that http get requests to "/greeting"  are mapped in mathod greeting
+    // RequestParam binds the value of the query parameter name into the name parameter of method
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model){
+        model.put("name", name);
+        return "greeting"; // Name of View. A View is responsible for rendering the HTML content. File name (greeting) that we want to display.
+    }
+
+    @GetMapping
+    public String root(Map<String, Object> model){
+        model.put("info", "Hi");
+        return "main";
+    }
+}
